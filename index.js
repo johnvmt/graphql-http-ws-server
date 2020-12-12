8f0b22c887fc6e99f51fcb381a0d3ff23132d07e
johnvmt <johnvmt@gmail.com>
2020-12-12 16:51:14 -0500

    v1.2.0 * Converted to type: module * Upgraded dependencies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import GraphQLHTTPWSServer from "./src/GraphQLHTTPWSServer";
+import GraphQLHTTPWSServer from "./src/GraphQLHTTPWSServer.js";
 export {GraphQLHTTPWSServer};
 export default GraphQLHTTPWSServer;
 export * from "apollo-server-express";
